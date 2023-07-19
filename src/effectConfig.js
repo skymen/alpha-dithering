dc6f2a64fc8bc7069841748d8890ccc34231f57d
Ossama <skymen75019@gmail.com>
2023-07-19 18:18:16 +0200

    Fixed WGSL

--- src/effectConfig.js
+++ src/effectConfig.js
@@ -3,7 +3,7 @@ module.exports = {
   addonType: "effect",
   id: "alphadither",
   name: "Alpha Dithering",
-  version: "1.0.0.4",
+  version: "1.0.0.5",
   category:
     // "blend",
     // "distortion",
